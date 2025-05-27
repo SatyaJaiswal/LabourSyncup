f010d7d76de101cd7699f9dde10ec8685731b848
Satya Jaiswal <satyajaiswal672@gmail.com>
2025-05-27 13:45:54 +0530

    Vercel add

--- Backend/server.js
+++ Backend/server.js
@@ -35,5 +35,8 @@ app.use('/api/attendance', attendanceRoutes);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// Export the app for Vercel serverless
+module.exports = app;
